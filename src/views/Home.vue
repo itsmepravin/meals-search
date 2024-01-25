0f9161f4b9f3fb192b80da2f7d65784c836a1b32
itsmepravin <singh.pravin19@yahoo.com>
2024-01-25 16:25:12 +0545

    TailwindCSS add

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
-    Home Page
-    <p>{{ meals }}</p>
+    <div class="flex p-8 justify-center">
+        <input type="text" class="rounded border-2 border-gray-200 w-full" placeholder="Search for Meals">
+    </div>
 </template>
 
 <script setup>
